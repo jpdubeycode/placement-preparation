e86f8ece32e4df804586f62c216853a0b187eb0e
jpdubeycode <jaidubeyiitm@gmail.com>
2026-04-08 22:45:59 +0530

    Optimize distinct numbers solution using unordered_set

---
+++ CSES_SOLUTION/problem-1621.cpp
@@ -0,0 +1,24 @@
+//Distinct Numbers
+
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+
+    set<int>s;
+
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        s.insert(a);
+    }
+
+    cout<<s.size();
+
+}
+
